584978187f278b205c77f6a897c9569a60f0d517
Edward Ahn <edhyah@gmail.com>
2015-05-18 23:49:32 +0900

    prelim code written to test out shift register

--- lightRider/lightRider.ino
+++ lightRider/lightRider.ino
@@ -1,6 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+const int SER = 8;
+const int LATCH = 9;
+const int CLK = 10;
 
+void setup() {
+  pinMode(SER, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  pinMode(CLK, OUTPUT);
+  
+  digitalWrite(LATCH, LOW);
+  shiftOut(SER, CLK, MSBFIRST, B10101010);
+  digitalWrite(LATCH, HIGH);
 }
 
 void loop() {
