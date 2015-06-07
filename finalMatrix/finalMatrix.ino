1ee1ae13b87ba3ced33e40a020528fb67e972607
Edward Ahn <edhyah@gmail.com>
2015-06-07 21:10:35 +0900

    modified finalMatrix.ino to test matrix

--- finalMatrix/finalMatrix.ino
+++ finalMatrix/finalMatrix.ino
@@ -2,13 +2,6 @@ const int SER = 8;
 const int LATCH = 9;
 const int CLK = 10;
 
-int seq[16] = {
-  B10000111, B10001011, B10001101, B10001110,
-  B01001110, B01001101, B01001011, B01000111,
-  B00100111, B00101011, B00101101, B00101110,
-  B00011110, B00011101, B00011011, B00010111
-};
-
 /*
 Define alphabet here
 */
@@ -30,16 +23,11 @@ void setup() {
 }
 
 void loop() {
-  for (int i = 0; i < 16; i++) {
-    digitalWrite(LATCH, LOW);
-    shiftOut(SER, CLK, LSBFIRST, seq[i]);
-    digitalWrite(LATCH, HIGH);
-    delay(100);
-  }
-  for (int i = 15; i >= 0; i--) {
-    digitalWrite(LATCH, LOW);
-    shiftOut(SER, CLK, LSBFIRST, seq[i]);
-    digitalWrite(LATCH, HIGH);
-    delay(100);
-  }
+  digitalWrite(LATCH, LOW);
+  shiftOut(SER, CLK, LSBFIRST, B10101010);
+  shiftOut(SER, CLK, LSBFIRST, 0);
+  shiftOut(SER, CLK, LSBFIRST, 0);
+  shiftOut(SER, CLK, LSBFIRST, 0);
+  digitalWrite(LATCH, HIGH);
+  delay(1);
 }
