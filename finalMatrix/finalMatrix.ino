b4ff4756c1cb09d46fc3b2212bdc5d63a3004ac8
Edward Ahn <edhyah@gmail.com>
2015-06-18 22:53:53 +0900

    defined A-D (alphabet)

--- finalMatrix/finalMatrix.ino
+++ finalMatrix/finalMatrix.ino
@@ -1,33 +1,52 @@
+/*
+ * Final 24x8 LED Matrix Code
+ * by Edward Ahn
+ *
+ */
+ 
+// Arduino Pins
 const int SER = 8;
 const int LATCH = 9;
 const int CLK = 10;
 
-/*
-Define alphabet here
-*/
+// Row Values
+const int ROWS[] = {1, 2, 4, 8, 16, 32, 64, 128};
 
-/*
-void display(String word) {
-  for (int i = 0; i < word.length(); i++) {
-    digitalWrite(LATCH, LOW);
-    shiftOut(SER, CLK, LSBFIRST, /* letter */);
-    digitalWrite(LATCH, HIGH);
-  }
-}
-*/
+// Alphabet
+#define A {B01110000, B10001000, B10001000, B10001000, B11111000, B10001000, \
+  B10001000, B10001000}
+#define B {B11110000, B10001000, B10001000, B11110000, B10001000, B10001000, \
+  B10001000, B11110000}
+#define C {B01110000, B10001000, B10000000, B10000000, B10000000, B10000000, \
+  B10001000, B01110000}
+#define D {
 
 void setup() {
   pinMode(SER, OUTPUT);
   pinMode(LATCH, OUTPUT);
   pinMode(CLK, OUTPUT);
+  Serial.begin(9600);
+  
+  /*
+  digitalWrite(LATCH, LOW);
+  shiftOut(SER, CLK, LSBFIRST, B11111111);
+  shiftOut(SER, CLK, LSBFIRST, B11111111);
+  shiftOut(SER, CLK, LSBFIRST, B01111111);
+  shiftOut(SER, CLK, MSBFIRST, 1);
+  digitalWrite(LATCH, HIGH);
+  */
 }
 
 void loop() {
-  digitalWrite(LATCH, LOW);
-  shiftOut(SER, CLK, LSBFIRST, B10101010);
-  shiftOut(SER, CLK, LSBFIRST, 0);
-  shiftOut(SER, CLK, LSBFIRST, 0);
-  shiftOut(SER, CLK, LSBFIRST, 0);
-  digitalWrite(LATCH, HIGH);
-  delay(1);
+  /*
+  for (int row = 0; row < 8; row++) {
+    digitalWrite(LATCH, LOW);
+    shiftOut(SER, CLK, LSBFIRST, B11111111);
+    shiftOut(SER, CLK, LSBFIRST, B11111111);
+    shiftOut(SER, CLK, LSBFIRST, B11011111);
+    shiftOut(SER, CLK, MSBFIRST, L[row]);
+    digitalWrite(LATCH, HIGH);
+    delay(1000);
+  }
+  */
 }
